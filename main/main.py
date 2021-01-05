b0521118920696202ae2670f58b91d713b9772f5
bossotron13 <bossotron13@gmail.com>
2021-01-05 17:47:10 -0500

    Now able to speak to server

---
+++ main/main.py
@@ -0,0 +1,39 @@
+'''
+This is a beta version of this software
+'''
+import subprocess
+import os
+import threading
+import time
+
+os.system("cls")
+
+'''
+This is hard coded for now
+'''
+
+executable = 'java -jar server.jar nogui'
+minecraft_dir = (
+    r'F:\Code\Python\New Projects\PyAdmin\server')
+
+
+def server_command(cmd):
+    global process
+    process.stdin.write(bytes(cmd+"\n", "utf-8"))
+    process.stdin.flush()
+    process.stdout.readline()
+
+def readserver():
+    global process
+    while True:
+        print(process.stdout.readline().decode('utf-8').strip("\n"))
+
+
+os.chdir(minecraft_dir)
+process = subprocess.Popen(executable, stdin=subprocess.PIPE, stdout=subprocess.PIPE)  # , stdout=subprocess.PIPE)
+threading.Thread(target=readserver).start()
+
+while True:
+    command = input()
+    command = command.lower()
+    threading.Thread(target=server_command, args=((command,))).start()
